c67575616fe5d3d2a2ece71804cf9544dd92df47
RifalArdi25 <rifalardi25@gmail.com>
2025-05-17 20:57:33 +0700

    Menyelesaikan coding

--- Latihan_Pertemuan9B.cpp
+++ Latihan_Pertemuan9B.cpp
@@ -13,7 +13,7 @@ class buku
     public:
     buku(string j, string p) : judul(j), penulis(p), dipinjam(false){}
     friend class petugas;
-
+    friend class admin;
 };
 
 class peminjam
@@ -37,13 +37,68 @@ class petugas
     string levelAkses;
 
     public:
-    petugas(string n, string ip) : nama(n), idPetugas(ip), levelAkses(0){}
+    petugas(string n, string ip, string lv) : nama(n), idPetugas(ip), levelAkses(lv){}
+    void prosesPinjam(buku& b, peminjam& p)
+    {
+        if (!b.dipinjam) 
+        {
+            b.dipinjam = true;
+            p.totalPinjaman++;
+            cout << "Buku berhasil dipinjam oleh " << p.nama << endl;
+        } 
+        else 
+        {
+            cout << "Buku sedang dipinjam.\n";
+        }
+    }
+
+        void prosesKembali(buku& b, peminjam& p) 
+    {
+        if (b.dipinjam) {
+            b.dipinjam = false;
+            p.totalPinjaman--;
+            cout << "Buku dikembalikan oleh " << p.nama << endl;
+        } else {
+            cout << "Buku belum dipinjam.\n";
+        }
+    }
+
     friend class admin;
 };
 
+
+
 class admin 
 {
-    
+   public:
+   void lihatStatistik(const peminjam &p, const buku &b)
+   {
+            cout << "Statistik Peminjam:\n";
+        cout << "- Nama: " << p.nama << "\n";
+        cout << "- Total Pinjaman: " << p.totalPinjaman << "\n";
+        cout << "- Status Buku: " << (b.dipinjam ? "Dipinjam" : "Tersedia") << "\n";
 
+   }
+   void ubahLevelPetugas(petugas &pgs, string levelBaru)
+    {
+        pgs.levelAkses = levelBaru;
+        cout << "Level akses petugas " << pgs.nama << " diubah menjadi " << levelBaru << endl;
+    }
 };
+
+int main()
+{
+    buku buku("Sejarah Bantul", "Gibran van Saput");
+    peminjam peminjam("Lik Haffi", "PM001");
+    petugas petugas("Bowo", "PT001", "standard");
+    admin admin;
+
+    petugas.prosesPinjam(buku, peminjam);
+    admin.lihatStatistik(peminjam, buku);
+    petugas.prosesKembali(buku, peminjam);
+    admin.ubahLevelPetugas(petugas, "senior");
+    admin.lihatStatistik(peminjam, buku);
+
+    return 0;
+}
 
