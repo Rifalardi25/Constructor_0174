188646ec3cd961ee37dc6122e3d20d4b949aa812
RifalArdi25 <rifalardi25@gmail.com>
2025-05-17 16:34:54 +0700

    Membuat akses modifer dan objek

--- Chainfunction.cpp
+++ Chainfunction.cpp
@@ -4,4 +4,16 @@ using namespace std;
 class buku
 {
    string judul; 
-};
+
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+
+}bukunya;
